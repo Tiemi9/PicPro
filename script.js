0e58676f05277d6ee14448ca453dcee03f0fbb8a
Tiemi9 <cristina_tiemi.social@yahoo.com>
2022-03-09 19:21:59 -0300

    revert commits

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //ocultar todos os campos desejados via CSS (display none)
 //manter somente a primeira div a mostra via CSS (div selecione pagamento)
 //adicionar ID nos botões em HTML
@@ -48,54 +47,3 @@ function removeClasseMostrar(){
 
 
 
-=======
-//ocultar todos os campos desejados via CSS (display none)
-//manter somente a primeira div a mostra via CSS (div selecione pagamento)
-//adicionar ID nos botões em HTML
-//1...4
-//selecionar os botões 
-//intercepitar o evento de clique
-//recuperar o id adicionado
-
-//selecionando a lista
-const listaOpcoes = document.querySelector(".secaoDetalhesdaConta_sobreTransacoes")
-
-//adicionando ecutador de eventos
-listaOpcoes.addEventListener("click", identificarOpcoes) 
-
-//executando a ação depois do click
-function identificarOpcoes(event) {
- 
-    //identificando local do click
-    const elemento = event.target
-    
-    //verificando se é li
-    if(elemento.tagName == "LI") {
-       
-    //recuperando o id
-    const id = elemento.id  
-    
-    //selecionando uma seção utilizando o ID (escolha do usuário)
-    const secaoEscolha = document.querySelector(`div[data-id="${id}"]`)
-                 
-      //REMOCENDO CLASSE MOSTRAR
-      removeClasseMostrar()
-    
-    //adicionar uma classe (mostrar)
-    secaoEscolha.classList.add("mostrar")
-    }
-}
-//FUNÇÃO PARA REMOVER AS CLASSES DAS DIVS  
-function removeClasseMostrar(){
-    const divs = document.querySelectorAll(".secaoTransacao .container div")
-    
-    for(let i = 0; i < divs.length; i++){
-
-        divs[i].classList.remove("mostrar")
-
-    }
-}
-
-
-
->>>>>>> 2245d4204e0ffaaa82a4a82b8f876d9d2d961a9a
